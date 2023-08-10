725758089fa62f6ea9a5c4ace95c4e59fe8968f2
David Taing <adavidtaing@gmail.com>
2023-08-10 22:08:01 +1000

    build: setup vitest and add Temporary Component
    Tests

---
+++ src/components/TestComponent.test.tsx
@@ -0,0 +1,7 @@
+import { screen, render } from "@testing-library/react";
+import { TestComponent } from "./TestComponent";
+
+test("TestComponent renders", () => {
+  render(<TestComponent />);
+  expect(screen.getByLabelText("Test Component")).toBeTruthy();
+})
